7333548732f1c5ca05fa4b3c688f681ddcb0d890
Andrei <vasileandrei.popa@gmail.com>
2025-04-11 11:19:56 +0300

    Added User model/model_tests and made migrations

--- app/app/settings.py
+++ app/app/settings.py
@@ -130,3 +130,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User' # set the model configuration, so all the models in our app will use the custom user model
