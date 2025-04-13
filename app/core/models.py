03b6181a002774c4b2f564f16b70d634fd73097d
Andrei <vasileandrei.popa@gmail.com>
2025-04-13 18:34:41 +0300

    Added tags API test&implement for Update tag & list tag

--- app/core/models.py
+++ app/core/models.py
@@ -56,6 +56,19 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
+    tags = models.ManyToManyField('tag')
 
     def __str__(self): # special method of a class that allows you to return the string representation of that obj
         return self.title # if you don't specify this it would just display the id of the object
+
+
+class Tag(models.Model):
+    """Tag for filtering recipes."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
