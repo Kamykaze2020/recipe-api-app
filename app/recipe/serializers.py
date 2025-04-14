b77703c77c9be0765d20699495ee8b6b45591e33
Andrei <vasileandrei.popa@gmail.com>
2025-04-14 14:04:35 +0300

    Added creating tag API feature and updating tags

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -9,13 +9,65 @@ from core.models import (
 )
 
 
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for recipe tags."""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
+
+
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
+    # for the nested serializer for the create tag feature
+    tags = TagSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
         read_only_fields = ['id']
+
+    # function added for update reicpe tags feature
+    # what we did is we took the functionality of create() because we'll need this funciton for both updating and creating
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting or creating tags as needed"""
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag,
+            )
+            recipe.tags.add(tag_obj)
+
+    # add custom so it's not read only
+    def create(self, validated_data):
+        """Create a recipe."""
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags, recipe)
+        # auth_user = self.context['request'].user
+        # for tag in tags:
+        #     tag_obj, created = Tag.objects.get_or_create(
+        #         user=auth_user,
+        #         **tag, # in place of name=tag['name'], because we want all values passed to the tag, so it's a futureproof
+        #     )
+        #     recipe.tags.add(tag_obj)
+
+        return recipe
+
+    def update(self, instance, validated_data):
+        """Update recipe."""
+        tags = validated_data.pop('tags', None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
@@ -23,12 +75,3 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
-
-
-class TagSerializer(RecipeSerializer):
-    """Serializer for recipe tags."""
-
-    class Meta:
-        model = Tag
-        fields = ['id', 'name']
-        read_only_fields = ['id']
